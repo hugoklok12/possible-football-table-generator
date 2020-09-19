93c5e1cfcc942164ae37c9e815af2e638b2c3422
Hugo Klok <hugodklok@gmail.com>
2020-09-19 11:41:33 +0200

    Setup env file + gitignore, update README, setup sample request

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # possible-football-table-generator
 Script that displays all possible changes in the table after a gameweek
+
+## Installation:
+- ```pip install python-dotenv```
+- Rename .env.example to .env and set the API_KEY value equal to a generated key from https://www.football-data.org
--- script.py
+++ script.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python
 import argparse
+import http.client
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+def sendRequest(requestArguments):
+    connection = http.client.HTTPConnection('api.football-data.org')
+    headers = { 'X-Auth-Token': API_KEY }
+    connection.request('GET', '/v2/' + requestArguments, None, headers)
+    response = json.loads(connection.getresponse().read().decode())
+    print (response)
+
 
 def Main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("club1", help="Enter a club", type=str)
-    args = parser.parse_args()
-    print(args.club1)
+
+    # get standings
+    sendRequest("competitions/2021/standings?standingType=HOME")
 
 if __name__ == "__main__":
     Main()
