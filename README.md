93501f21542279aa6c6fd0649d7df7f65f416850
Hugo Klok <hugodklok@gmail.com>
2020-09-19 13:44:18 +0200

    Created getter functions

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Script that displays all possible changes in the standings table after a played 
 ## Installation:
 - ```pip install python-dotenv```
 - Rename .env.example to .env and set the API_KEY value equal to a generated key from https://www.football-data.org
+
+## More information
+- The integer 2021 is the league identifier for the Premier League.
--- script.py
+++ script.py
@@ -8,18 +8,33 @@ load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 
-def sendRequest(requestArguments):
+def send_request(endpoint):
     connection = http.client.HTTPConnection('api.football-data.org')
     headers = { 'X-Auth-Token': API_KEY }
-    connection.request('GET', '/v2/' + requestArguments, None, headers)
+    connection.request('GET', '/v2/' + endpoint, None, headers)
     response = json.loads(connection.getresponse().read().decode())
-    print (response)
+    return response
 
+def get_entered_matchday():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('matchday', help='Enter the matchday', type=int)
+    args = parser.parse_args()
+    return str(args.matchday)
+
+def get_current_table():
+    response = send_request('competitions/2021/standings?standingType=HOME')
+    current_table = response['standings'][0]['table']
+    return current_table
+
+def get_matchday_matches(matchday):
+    response = send_request('competitions/2021/matches?matchday=' + matchday)
+    matchday_matches = response['matches']
+    return matchday_matches
 
 def Main():
+    matchday = get_entered_matchday()
+    current_table = get_current_table()
+    matchday_matches = get_matchday_matches(matchday)
 
-    # get standings
-    sendRequest("competitions/2021/standings?standingType=HOME")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     Main()
